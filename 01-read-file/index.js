125f85384c068464ea4cdd4877551b366505252b
gordinski <e.gordinski@gmail.com>
2023-05-06 11:13:14 +0300

    feat: complete '01-read-file' task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = require('process');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8')
+
+stream.on('data', (data) => stdout.write(data))
+stream.on('error', (err) => stdout.write('Error: ', err.message))
