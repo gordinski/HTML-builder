56ab8e031dd0767e018015ea2e30ba54e6edbec4
gordinski <e.gordinski@gmail.com>
2023-05-08 10:41:56 +0300

    feat: complete '04-copy-directory' task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
 
+fs.mkdir(destDir, { recursive: true }, (err) => {
+  if (err) throw err;
+  fs.readdir(destDir, { withFileTypes: true }, (err, arr) => {
+    if (err) {
+      throw err;
+    } else {
+      const files = arr
+        .filter(file => file.isFile())
+        .map(file => file.name);
+
+      files.forEach((file) => {
+        const deleteFile = path.join(destDir, file);
+
+        fs.unlink(deleteFile, (err) => {
+          if (err) throw err;
+        });
+      })
+    }
+
+    fs.readdir(srcDir, { withFileTypes: true }, (err, arr) => {
+      if (err) {
+        throw err;
+      } else {
+        const files = arr
+          .filter(file => file.isFile())
+          .map(file => file.name);
+
+        files.forEach((file) => {
+          const oldFile = path.join(srcDir, file);
+          const newFile = path.join(destDir, file);
+
+          fs.copyFile(oldFile, newFile, (err) => {
+            if (err) throw err;
+          });
+        })
+      }
+    });
+  });
+});
